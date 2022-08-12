bfb1f9e436d7f94d3b35a31ad04b95ec9251141c
fukuinaoki <fukuinaoki@team-lab.com>
2022-08-12 16:49:20 +0900

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TOREKIRO</title>
+        <title>TOREKIROtest</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
         <link rel="stylesheet" href="main.css">
