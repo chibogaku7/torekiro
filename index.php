87d43e3f6e628862f761cb8d86aa41c86c2863ec
chibogaku7 <chibogaku717@gmail.com>
2022-08-12 16:51:53 +0900

    testcommit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>TOREKIROtesttt</title>
+        <title>TOREKIRO1</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
         <link rel="stylesheet" href="main.css">
