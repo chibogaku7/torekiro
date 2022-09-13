1751a721d7e60e23adc37782925ff6d33529d3bb
ToyaKasai <kasaitoya@team-lab.com>
2022-09-13 22:00:36 +0900

    feat: add sample component

--- resources/views/pages/sample/index.blade.php
+++ resources/views/pages/sample/index.blade.php
@@ -2,5 +2,10 @@
 
 @section('content')
     <h1>SAMPLE PAGE</h1>
+    <x-Sample.component title="サンプルコンポーネント">
+        <x-slot name="contents">
+            コンテンツ
+        </x-slot>
+    </x-Sample.component>
 @endsection
 
