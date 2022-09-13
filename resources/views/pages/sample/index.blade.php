6241b632c17f2137ada63e2c675674d9bf7d69d4
ToyaKasai <kasaitoya@team-lab.com>
2022-09-13 22:01:11 +0900

    fix: case

--- resources/views/pages/sample/index.blade.php
+++ resources/views/pages/sample/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <h1>SAMPLE PAGE</h1>
-    <x-Sample.component title="サンプルコンポーネント">
+    <x-sample.component title="サンプルコンポーネント">
         <x-slot name="contents">
             コンテンツ
         </x-slot>
-    </x-Sample.component>
+    </x-sample.component>
 @endsection
 
