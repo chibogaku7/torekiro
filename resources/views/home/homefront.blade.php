a9542646a7f374aacac7b82f14a24bd7885049c0
yokosekenshin <62970960+yokosekenshin@users.noreply.github.com>
2022-09-27 13:01:22 +0900

    finish

--- resources/views/home/homefront.blade.php
+++ resources/views/home/homefront.blade.php
@@ -11,13 +11,14 @@
         <div class="home_form_wrapper">
             <div class="home_main_wrapper">
                 <p class="home_main_title">TOREKIRO</p>
-                <p class="home_main_text">確かな成長を記録にaaa</p>
+                <p class="home_main_text">確かな成長を記録に</p>
                 <img src="{{asset('assets/img/torekiro_logo_black.png')}}" class="main_img">
             </div>
 
 
-            <div id="home_botton_wrapper">
-                <a class="login_form_submit" href="https://google.com/">トレーニングを始める</a>
+            <div class="home_botton_wrapper">
+                <a class="home_start_training_form_submit" href="https://google.com/">トレーニングを始める</a>
+                <a class="home_view_history_form_submit" href="https://google.com/">記録を見る</a>
             </div>
         <div>
    </body>
